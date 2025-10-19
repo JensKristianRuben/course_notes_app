954dff545425abc9b372f62d6fbc9dda2edc00e3
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-19 16:38:17 +0200

    everything back in app.js

--- app.js
+++ app.js
@@ -2,36 +2,92 @@ import express from "express";
 import fs from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import { readAndParseMarkdownFiles, readAndParseMarkdownFile } from "./util/markdown.js";
-
+import { marked } from "marked";
+import hljs from "highlight.js";
+import { JSDOM } from "jsdom";
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url); 
-const __dirname = dirname(__filename); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //=========================================MARKDOWN FILES=============================
 
-    const markdownHtmlFiles = readAndParseMarkdownFiles();
-    const readmeHtml = readAndParseMarkdownFile();
+const markdownHtmlFiles = readAndParseMarkdownFiles();
+const readmeHtml = readAndParseMarkdownFile();
+
+function readAndParseMarkdownFiles() {
+  const htmlFiles = {};
+  const markdownPath = path.join(__dirname, "markdown");
+  const markdownFileNames = fs.readdirSync(markdownPath);
+
+  for (const file of markdownFileNames) {
+    const markdownFileString = fs.readFileSync(
+      path.join(markdownPath, file),
+      "utf-8"
+    );
+    const markdownFileStringToHTML = marked.parse(markdownFileString);
+    const highlightedHtmlFiles = highlightHtml(markdownFileStringToHTML);
+    htmlFiles[file] = highlightedHtmlFiles;
+  }
+
+  return htmlFiles;
+}
+
+function readAndParseMarkdownFile() {
+  const readmePath = path.join(__dirname, "README.md");
+  const readmeFileString = fs.readFileSync(readmePath, "utf-8");
+  const readmeFileStringToHtml = marked.parse(readmeFileString);
+
+  const highlightedHtml = highlightHtml(readmeFileStringToHtml);
+
+  return highlightedHtml;
+}
+
+function highlightHtml(htmlString) {
+  const dom = new JSDOM(htmlString);
+  const codes = dom.window.document.querySelectorAll("pre code");
+
+  codes.forEach((codeElement) => {
+    const langClass = codeElement.className || "";
+    const lang = langClass.replace("language-", "") || undefined;
+
+    let highlighted;
+
+    if (lang && hljs.getLanguage(lang)) {
+      highlighted = hljs.highlight(codeElement.textContent, {
+        language: lang,
+        ignoreIllegals: true,
+      }).value;
+    } else {
+      highlighted = hljs.highlightAuto(codeElement.textContent).value;
+    }
+
+    codeElement.innerHTML = highlighted;
+    if (!codeElement.classList.contains("hljs"))
+      codeElement.classList.add("hljs");
+  });
+
+  return dom.serialize();
+}
 
 // ========================================PAGES========================================
 
-    const indexPagePath = path.join(__dirname, "public", "index.html");
-    const fourZeroFourPagePath = path.join(__dirname, "public", "404.html");
+const indexPagePath = path.join(__dirname, "public", "index.html");
+const fourZeroFourPagePath = path.join(__dirname, "public", "404.html");
 
-    const indexPage = fs.readFileSync(indexPagePath, "utf-8");
-    const fourZeroFourPage = fs.readFileSync(fourZeroFourPagePath, "utf-8");
+const indexPage = fs.readFileSync(indexPagePath, "utf-8");
+const fourZeroFourPage = fs.readFileSync(fourZeroFourPagePath, "utf-8");
 
 app.get("/markdown/:file", async (req, res) => {
   const fileName = req.params.file;
-  const htmlToSend = markdownHtmlFiles[fileName]  
+  const htmlToSend = markdownHtmlFiles[fileName];
 
-
-  if(!fs.existsSync(path.join(__dirname, "markdown", fileName))){; // tjekker om filen eksisterer {
+  if (!fs.existsSync(path.join(__dirname, "markdown", fileName))) {
+    // tjekker om filen eksisterer {
     return res.status(404).send(fourZeroFourPage);
   }
-   
+
   const indexPageToSend = indexPage
     .replace("$$MARKDOWNCONTENT$$", htmlToSend)
     .replace("$$TITLE$$", `Course_notes_app | ${fileName}`);
@@ -39,28 +95,23 @@ app.get("/markdown/:file", async (req, res) => {
   res.send(indexPageToSend);
 });
 
-
 app.get("/", (req, res) => {
-
   const indexPageToSend = indexPage
-  .replace("$$MARKDOWNCONTENT$$", readmeHtml)
-  .replace("$$TITLE$$", "Course_notes_app")
+    .replace("$$MARKDOWNCONTENT$$", readmeHtml)
+    .replace("$$TITLE$$", "Course_notes_app");
 
   res.send(indexPageToSend);
 });
-
-
 
 // app.use() skal ligge under "/" endpointet - ellers læser express index filen før den muteres
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public", "images")));
 
-
 // ========================================CONFIG========================================
 const PORT = Number(process.env.PORT);
 
-
-
 app.listen(PORT, () => {
-    console.log(`🚀🚀🚀🚀🚀🚀🚀🚀 Server is running on port ${PORT} 🚀🚀🚀🚀🚀🚀🚀🚀`);
-  });
+  console.log(
+    `🚀🚀🚀🚀🚀🚀🚀🚀 Server is running on port ${PORT} 🚀🚀🚀🚀🚀🚀🚀🚀`
+  );
+});
--- util/markdown.js
+++ util/markdown.js
@@ -1,62 +1,62 @@
-import fs from "fs";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import { marked } from "marked";
-import hljs from "highlight.js";
-import { JSDOM } from "jsdom";
+// import fs from "fs";
+// import path, { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { marked } from "marked";
+// import hljs from "highlight.js";
+// import { JSDOM } from "jsdom";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-export function readAndParseMarkdownFiles() {
-  const htmlFiles = {};
-  const markdownPath = path.join(__dirname, "../markdown");
-  const markdownFileNames = fs.readdirSync(markdownPath);
+// export function readAndParseMarkdownFiles() {
+//   const htmlFiles = {};
+//   const markdownPath = path.join(__dirname, "../markdown");
+//   const markdownFileNames = fs.readdirSync(markdownPath);
 
-  for (const file of markdownFileNames) {
-    const markdownFileString = fs.readFileSync(path.join(markdownPath, file),"utf-8");
-    const markdownFileStringToHTML = marked.parse(markdownFileString);
-    const highlightedHtmlFiles = highlightHtml(markdownFileStringToHTML);
-    htmlFiles[file] = highlightedHtmlFiles;
-  }
+//   for (const file of markdownFileNames) {
+//     const markdownFileString = fs.readFileSync(path.join(markdownPath, file),"utf-8");
+//     const markdownFileStringToHTML = marked.parse(markdownFileString);
+//     const highlightedHtmlFiles = highlightHtml(markdownFileStringToHTML);
+//     htmlFiles[file] = highlightedHtmlFiles;
+//   }
 
 
-  return htmlFiles;
-}
+//   return htmlFiles;
+// }
 
-export function readAndParseMarkdownFile() {
-  const readmePath = path.join(__dirname, "../README.md");
-  const readmeFileString = fs.readFileSync(readmePath, "utf-8");
-  const readmeFileStringToHtml = marked.parse(readmeFileString);
+// export function readAndParseMarkdownFile() {
+//   const readmePath = path.join(__dirname, "../README.md");
+//   const readmeFileString = fs.readFileSync(readmePath, "utf-8");
+//   const readmeFileStringToHtml = marked.parse(readmeFileString);
 
-  const highlightedHtml = highlightHtml(readmeFileStringToHtml);
+//   const highlightedHtml = highlightHtml(readmeFileStringToHtml);
 
-  return highlightedHtml;
-}
+//   return highlightedHtml;
+// }
 
-function highlightHtml(htmlString) {
-  const dom = new JSDOM(htmlString);
-  const codes = dom.window.document.querySelectorAll("pre code");
+// function highlightHtml(htmlString) {
+//   const dom = new JSDOM(htmlString);
+//   const codes = dom.window.document.querySelectorAll("pre code");
 
-  codes.forEach((codeElement) => {
-    const langClass = codeElement.className || "";
-    const lang = langClass.replace("language-", "") || undefined;
+//   codes.forEach((codeElement) => {
+//     const langClass = codeElement.className || "";
+//     const lang = langClass.replace("language-", "") || undefined;
 
-    let highlighted;
+//     let highlighted;
 
-    if (lang && hljs.getLanguage(lang)) {
-      highlighted = hljs.highlight(codeElement.textContent, {
-        language: lang,
-        ignoreIllegals: true,
-      }).value;
-    } else {
-      highlighted = hljs.highlightAuto(codeElement.textContent).value;
-    }
+//     if (lang && hljs.getLanguage(lang)) {
+//       highlighted = hljs.highlight(codeElement.textContent, {
+//         language: lang,
+//         ignoreIllegals: true,
+//       }).value;
+//     } else {
+//       highlighted = hljs.highlightAuto(codeElement.textContent).value;
+//     }
 
-    codeElement.innerHTML = highlighted;
-    if (!codeElement.classList.contains("hljs"))
-      codeElement.classList.add("hljs");
-  });
+//     codeElement.innerHTML = highlighted;
+//     if (!codeElement.classList.contains("hljs"))
+//       codeElement.classList.add("hljs");
+//   });
 
-  return dom.serialize();
-}
+//   return dom.serialize();
+// }
